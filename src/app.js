31eb79e687dee10181fba21f391f7f38c632e945
Emilia Brzoskowska <emilia.brzoskowska@gmail.com>
2021-07-24 09:33:50 +0100

    fixed add movie functionality

--- src/app.js
+++ src/app.js
@@ -2,18 +2,21 @@ require("./db/connection");
 const yargs = require("yargs");
 const { addUser } = require("./utils");
 const { showData } = require("./utils");
+const { addMovie } = require("./utils");
 const command = process.argv[2];
 const user = yargs.argv.user;
 const password = yargs.argv.password;
 const title = yargs.argv.title;
+const categories = yargs.argv.categories;
 const actor = yargs.argv.actor;
 const watched = yargs.argv.watched;
+const rating = yargs.argv.rating;
 
 const app = () => {
     if(command === 'add user') {
         addUser(user, password)
     } else if (command === 'add movie') {
-        //add movie function from utils
+        addMovie(title, categories, actor, watched, rating)
     } else if (command === 'show data') {
         showData()
     }
--- src/utils/index.js
+++ src/utils/index.js
@@ -3,59 +3,57 @@ const sql = require("../db/connection");
 exports.addUser = (username, password) => {
   try 
   {
-      const user = {
-        username: username,
-        password: password
-      }
-      sql.query("INSERT INTO users SET ?", user) 
-// insert into users set username = "Cholerka", pass = 'jasna', it works different with many tables:        
-// ("INSERT INTO users SET username='?'; INSERT INTO password SET password='?', userID = (SELECT is FROM users WHERE username = '?')",user)
+    const user = {
+      username: username,
+      password: password
+    }
+    sql.query("INSERT INTO users SET ?", user) 
   } 
   catch (error)
   {
-      console.log(error)
+    console.log(error)
   }
 }
 
 exports.addMovie = (title, categories, actor, watched, rating) => {
-    try 
-      {
-        const movie = {
-          title: title,
-          categories: categories,
-          actor: actor, 
-          watched: watched, 
-          rating: rating    
-        }
-        sql.query("INSERT INTO users SET ?", movie) 
-      } 
-    catch (error)
-      {
-        console.log(error)
-      }
+  try 
+  {
+    const movie = {
+      title: title,
+      categories: categories,
+      actor: actor, 
+      watched: watched, 
+      rating: rating    
+    }
+    sql.query("INSERT INTO movies SET ?", movie) 
+  } 
+  catch (error)
+  {
+    console.log(error)
+  }
 }
 
 exports.showData = () => {
   try 
-    {
-      sql.query("SELECT title, categories, actor, watched, rating, username, password FROM movies, users WHERE movies.userID = users.id", function(err, result){     
-        console.table(
-            result.map(command => {
-                return {
-                    "title": command.title,
-                    "category": command.categories,
-                    "actor": command.actor,
-                    "watched": command.watched,
-                    "rating": command.rating,
-                    "username": command.username,
-                    "password": command.password
-                };
-            })
-        );        
-      })
-    } 
+  {
+    sql.query("SELECT title, categories, actor, watched, rating, username, password FROM movies, users WHERE movies.userID = users.id", function(err, result){     
+      console.table(
+        result.map(command => {
+          return {
+            "title": command.title,
+            "category": command.categories,
+            "actor": command.actor,
+            "watched": command.watched,
+            "rating": command.rating,
+            "username": command.username,
+            "password": command.password
+          };
+        })
+      );        
+    })
+  } 
   catch (error)
-    {
-      console.log(error)
-    }
+  {
+    console.log(error)
+  }
 }
